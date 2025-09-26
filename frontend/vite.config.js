5187f5bbe8c9fce7053c64146e5a459e7de68f94
MohammadM01 <mullamohammad123@gmail.com>
2025-09-26 16:19:52 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,12 +11,20 @@ export default defineConfig({
     },
   },
   server: {
+    host: "0.0.0.0",
     port: 3000,
+    allowedHosts: [
+      "civic-pg9s.onrender.com" // 👈 Render domain allowed
+    ],
     proxy: {
       "/api": {
         target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
   },
 })
