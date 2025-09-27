47f94dfbed0ae885e5e3ae658245bba0c9f27441
MohammadM01 <mullamohammad123@gmail.com>
2025-09-27 19:27:30 +0530

    SIH ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
-    port: 3000,
+    port: process.env.PORT || 3000,
     allowedHosts: [
       "civic-pg9s.onrender.com" // 👈 Render domain allowed
     ],
@@ -25,6 +25,6 @@ export default defineConfig({
   },
   preview: {
     host: "0.0.0.0",
-    port: 3000,
+    port: process.env.PORT || 3000,
   },
 })
